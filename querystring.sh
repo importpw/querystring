6114702fd058f1e7765c51e90d3672846dbd5dd9
Nathan Rajlich <nathan@tootallnate.net>
2018-07-20 20:18:05 -0700

    Add encode tests for the entire ASCII table
    With values matching what Node.js `querystring.escape()` outputs.
    
    Also adds `stdin` support to `querystring_encode`

--- querystring.sh
+++ querystring.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 querystring_escape() {
-  local input="$*"
+  local input=
+  if [ $# -gt 0 ]; then
+    input="$*"
+  else
+    # assume stdin
+    input="$(cat)"
+  fi
   local length="${#input}"
   for (( i = 0; i < length; i++ )); do
     local c="${input:i:1}"
-    case $c in
-      [a-zA-Z0-9.~_-]) printf "$c" ;;
-      *) printf '%%%02X' "'$c"
-    esac
+    if [ "$c" = "!" ] || [ "$c" = "(" ] || [ "$c" = ")" ] || [ "$c" = "'" ] || [ "$c" = "*" ]; then
+      printf "$c"
+    else
+      case $c in
+        [a-zA-Z0-9.~_-]) printf "$c" ;;
+        *) printf '%%%02X' "'$c"
+      esac
+    fi
   done
 }
 
